55c973b531e0c152365f838fdef7604a57dca518
Chidananda Gonuguntla <chidha2005@gmail.com>
2026-04-26 00:24:03 -0400

    initial commit

---
+++ fraud-risk-service/src/main/java/com/system/design/fraud/risk/service/FraudRiskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.system.design.fraud.risk.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudRiskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudRiskServiceApplication.class, args);
+	}
+
+}
---
+++ fraud-risk-service/src/test/java/com/system/design/fraud/risk/service/FraudRiskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.design.fraud.risk.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudRiskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
